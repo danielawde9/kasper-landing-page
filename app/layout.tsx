e30fd10a1db481c6ea8e3fc08d36daa98fefb41a
danielawdelelabo <148767660+danielawdelelabo@users.noreply.github.com>
2024-08-31 13:51:37 +0300

    few fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/layout/navbar";
 import { ThemeProvider } from "@/components/layout/theme-provider";
+import { Analytics } from "@vercel/analytics/react"; // Import Vercel Analytics
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -12,13 +14,13 @@ export const metadata: Metadata = {
     "Kasper provides tailored digital marketing training and strategies to help small, community-focused businesses thrive online.",
   openGraph: {
     type: "website",
-    url: "https://your-kasper-website.com", // Replace with Kasper's actual URL
+    url: "https://kasperbythesisters.com",
     title: "Kasper - Empowering Small Businesses with Digital Marketing",
     description:
       "Kasper offers expert digital marketing training and strategies to help small businesses grow their online presence.",
     images: [
       {
-        url: "https://your-kasper-website.com/og-image.jpg", // Replace with the actual image URL for OG
+        url: "https://kasperbythesisters.com/og-image.png", // Replace with the actual image URL for OG
         width: 1200,
         height: 630,
         alt: "Kasper - Digital Marketing for Small Businesses",
@@ -27,13 +29,11 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    site: "https://your-kasper-website.com", // Replace with Kasper's actual URL
+    site: "https://kasperbythesisters.com",
     title: "Kasper - Empowering Small Businesses with Digital Marketing",
     description:
       "Learn how Kasper's digital marketing strategies can help your small business succeed online.",
-    images: [
-      "https://your-kasper-website.com/og-image.jpg", // Replace with the actual image URL for Twitter
-    ],
+    images: ["https://kasperbythesisters.com/og-image.png"],
   },
   icons: {
     icon: "/logo.png",
@@ -58,6 +58,7 @@ export default function RootLayout({
 
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
